c5eb54c2cc715ef51dec99d4e6730b7687112334
Dirk Brand <dirkbrnd@gmail.com>
2025-02-12 15:36:24 +0200

    Update

--- api/settings.py
+++ api/settings.py
@@ -48,6 +48,7 @@ class ApiSettings(BaseSettings):
                 "http://localhost:3000",
                 "https://app.agno.com",
                 "https://app-stg.agno.com",
+                # Add more here if you are not hosting on localhost
             ]
         )
 
